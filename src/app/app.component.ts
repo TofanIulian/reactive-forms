f95cff4c1bbc1d968c1c4622aae712e664e2694a
Iulian Tofan <itn@tanacorp.com>
2022-08-31 00:00:00 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'reactive-forms';
+
+  profileForm = new FormGroup({
+    bookingGuest: new FormGroup({
+      firstName: new FormControl(''),
+      lastName: new FormControl(''),
+      phone: new FormControl(''),
+    }),
+    guests: new FormArray<FormArray>([
+      new FormArray([
+        new FormGroup({
+          firstName: new FormControl(''),
+          lastName: new FormControl(''),
+        }),
+        new FormGroup({
+          firstName: new FormControl(''),
+          lastName: new FormControl(''),
+        }),
+      ]),
+      new FormArray([
+        new FormGroup({
+          firstName: new FormControl(''),
+          lastName: new FormControl(''),
+        }),
+      ]),
+    ]),
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {}
+
+  onSubmit() {
+    console.warn(this.profileForm.value);
+  }
+
+  get guestControls() {
+    return (this.profileForm.get('guests') as FormArray)
+      .controls as FormArray[];
+  }
 }
